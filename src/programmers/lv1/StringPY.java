72ba5172aae769974e33de69f83da396427bd57d
ChoiRang <jkchldyd@gmail.com>
2021-12-06 00:56:22 +0900

    reUpdate

--- src/programmers/lv1/StringPY.java
+++ src/programmers/lv1/StringPY.java
@@ -1,5 +1,5 @@
 package programmers.lv1;
-
+/**문자열 내 p와 y의 개수*/
 public class StringPY {
 	boolean solution(String value) {
 		String lowerValue = value.toLowerCase();
@@ -15,13 +15,4 @@ public class StringPY {
 		return inputString.replace(deleteOfIndex, "").length();
 	}
 }
-//-------------------
-//실패잇음, 다시보니 크기가 같음
-//String ss1 = s.replaceAll("p", "");
-//String ss2 = s.replaceAll("y", "");
-//System.out.println(ss1);
-//System.out.println(ss2);
-//if(ss1.length() == ss2.length()) {
-//	return true;
-//}
-//return false;
+//-------------------\
